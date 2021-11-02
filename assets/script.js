172072ea0709a31821489ac726857d940adf23ff
superTyDev <tysonm2496@mypolkschools.net>
2021-11-02 16:09:16 +0000

    added animations

--- assets/script.js
+++ assets/script.js
@@ -17,10 +17,10 @@ function responsiveNavbar() {
 }
 
 var slideIndex = 1;
-showSlides(slideIndex);
-setInterval(function () {
+var slideInterval = setInterval(function () {
 	plusSlides(1);
 }, 10000);
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -40,5 +40,42 @@ function showSlides(n) {
 		slides[i].style.display = "none";
 	}
 	slides[slideIndex - 1].style.display = "block";
+	clearInterval(slideInterval);
+	slideInterval = setInterval(function () {
+		plusSlides(1);
+	}, 10000);
 }
+
+var p = $('.parallax p');
+console.log(p)
+var animator = $('<span class="anim_show"></span>');
+var hidden = $('<span class="hid"></span>')
+var textLength = p.text().length;
+var offset = 0;
+
+function animate(letter_position) {
+  var text = p.text();
+  p.text('');
+  var textBefore = text.substring(0, letter_position - 1);
+  var textAfter = text.substring(letter_position, textLength);
+  var letter = text.substr(letter_position - 1, 1);
+  animator.text(letter);
+  hidden.text(textAfter);
+  p.append(textBefore);
+  p.append(animator);
+  p.append(hidden);
+}
+
+var interval = setInterval(function() {
+  if (p.hasClass('hid')) p.removeClass('hid');
+  if (offset - 2 === textLength) {
+    clearInterval(interval);
+    p.children('.anim').remove();
+    p.children('.hid').remove();
+    return;
+  }
+  animate(offset);
+  offset++;
+}, 100);
+
 
