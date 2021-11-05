ec94db28bf22466885a76dc92ae7d52a965d2e36
superTyDev <tysonm2496@mypolkschools.net>
2021-11-05 16:07:56 +0000

    update styles

--- assets/modern_script.js
+++ assets/modern_script.js
@@ -1,28 +1,32 @@
-window.onscroll = function () { myFunction() };
-
 // Get the navbar
 var navbar = document.getElementsByTagName("nav")[0];
 var header = document.getElementsByTagName("header")[0];
+var sticky = navbar.offsetTop;
+
+window.onscroll = function () { stickyNav() };
+window.addEventListener('resize', placeNav);
+
+placeNav();
+
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 function placeNav(event) {
 	header.style.height = (parseInt(window.innerHeight) - parseInt(navbar.offsetHeight)).toString() + "px";
 	console.log(header.style.height);
 	// navbar.style.height = navbar.offsetHeight + "px";
 	sticky = navbar.offsetTop;
 }
-placeNav();
-window.addEventListener('resize', placeNav);
-
-
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyNav() {
 	if (window.pageYOffset >= sticky) {
 		navbar.classList.add("sticky")
 	} else {
 		navbar.classList.remove("sticky");
+	}
+
+    if (window.pageYOffset == 0) {
+		navbar.classList.add("reverse")
 	}
 }
 
