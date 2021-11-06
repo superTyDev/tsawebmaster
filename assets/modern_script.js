61941def5fce518ce7d98d48fe430c813ffe7cab
superTyDev <tysonm2496@mypolkschools.net>
2021-11-06 19:05:48 -0400

    style updates

--- assets/modern_script.js
+++ assets/modern_script.js
@@ -22,7 +22,6 @@ function placeNav(event) {
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
-	console.log(`${window.pageYOffset} >= ${sticky}`);
 	if (window.pageYOffset >= sticky) {
 		navbar.classList.add("sticky");
 	} else {
@@ -51,7 +50,6 @@ function responsiveNavbar() {
 
 showSlides(slideIndex);
 
-// Next/previous controls
 function nextSlide() {
 	showSlides((slideIndex += 1));
 }
