16b651eedd7c80c3578bde7e11eca347fad6126a
superTyDev <tysonm2496@mypolkschools.net>
2021-11-06 16:17:49 -0400

    value adjustment

--- assets/modern_script.js
+++ assets/modern_script.js
@@ -3,37 +3,45 @@ var navbar = document.getElementsByTagName("nav")[0];
 var header = document.getElementsByTagName("header")[0];
 var sticky = navbar.offsetTop;
 
-window.onscroll = function () { stickyNav() };
-window.addEventListener('resize', placeNav);
+window.onscroll = function () {
+	stickyNav();
+};
+window.addEventListener("resize", placeNav);
 
 placeNav();
 
 // Get the offset position of the navbar
 function placeNav(event) {
-    header.style.height = (parseInt(window.innerHeight) - parseInt(navbar.offsetHeight)).toString() + "px";
-    console.log(header.style.height);
-    sticky = navbar.offsetTop;
+	header.style.height =
+		(
+			parseInt(window.innerHeight) -
+			parseInt(navbar.offsetHeight) -
+			5
+		).toString() + "px";
+	console.log(header.style.height);
+	sticky = navbar.offsetTop;
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
 
-    if (window.pageYOffset == 0) {
-        navbar.classList.add("reverse");
-    } else {
-        navbar.classList.remove("reverse");
-    }
+	console.log(window.pageYOffset);
+	if (window.pageYOffset <= 200) {
+		navbar.classList.add("reverse");
+	} else {
+		navbar.classList.remove("reverse");
+	}
 }
 
 function responsiveNavbar() {
-    if (navbar.classList.contains("responsive")) {
-        navbar.classList.remove("responsive");
-    } else {
-        navbar.classList.add("responsive");
-    }
+	if (navbar.classList.contains("responsive")) {
+		navbar.classList.remove("responsive");
+	} else {
+		navbar.classList.add("responsive");
+	}
 }
