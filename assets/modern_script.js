b1a5796105b0056ea78c62a7f8140fe41c222112
superTyDev <tysonm2496@mypolkschools.net>
2021-11-06 18:36:44 -0400

    added header

--- assets/modern_script.js
+++ assets/modern_script.js
@@ -1,30 +1,39 @@
 // Get the navbar
 var navbar = document.getElementsByTagName("nav")[0];
 var header = document.getElementsByTagName("header")[0];
+
 var sticky = navbar.offsetTop;
+
+var slideIndex = 1;
+var slideInterval = setInterval(nextSlide, 1000);
 
 window.onscroll = function () {
 	stickyNav();
 };
 window.addEventListener("resize", placeNav);
-window.onload = setTimeout(placeNav, 100);
+window.onload = setTimeout(placeNav, 300);
 
 // Get the offset position of the navbar
 function placeNav(event) {
 	header.style.height =
-		(parseInt(window.innerHeight) - parseInt(navbar.offsetHeight)).toString() +
-		"px";
-	sticky = navbar.offsetTop;
+		(window.innerHeight - navbar.offsetHeight).toString() + "px";
+	sticky = window.innerHeight - navbar.offsetHeight - 5;
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
+	console.log(`${window.pageYOffset} >= ${sticky}`);
 	if (window.pageYOffset >= sticky) {
 		navbar.classList.add("sticky");
 	} else {
 		navbar.classList.remove("sticky");
 	}
 
+	if (window.pageYOffset == 0) {
+		navbar.classList.remove("shrink");
+	} else {
+		navbar.classList.add("shrink");
+	}
 	if (window.pageYOffset <= 200) {
 		navbar.classList.add("reverse");
 	} else {
@@ -38,4 +47,29 @@ function responsiveNavbar() {
 	} else {
 		navbar.classList.add("responsive");
 	}
+}
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function nextSlide() {
+	showSlides((slideIndex += 1));
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
+
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide, 5000);
 }
